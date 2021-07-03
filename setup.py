33305c1babe787f88984811d481a66b6e13b1c30
Zoe Martin <git@zoemartin.me>
2021-07-03 11:23:13 +0200

    feat: init repo with cookiecutter

---
+++ setup.py
@@ -0,0 +1,61 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'plaster_pastedeploy',
+    'pyramid',
+    'pyramid_mako',
+    'pyramid_debugtoolbar',
+    'waitress',
+    'alembic',
+    'pyramid_retry',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+]
+
+tests_require = [
+    'WebTest',
+    'pytest',
+    'pytest-cov',
+]
+
+setup(
+    name='simple_blog',
+    version='0.0',
+    description='Simple Blog',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = simple_blog:main',
+        ],
+        'console_scripts': [
+            'initialize_simple_blog_db=simple_blog.scripts.initialize_db:main',
+        ],
+    },
+)
