22834443b363d4470992313ff7215ef5ae69895d
Zoe Martin <git@zoemartin.me>
2021-07-04 12:01:48 +0200

    feat: added post routing

--- simple_blog/routes.py
+++ simple_blog/routes.py
@@ -1,3 +1,7 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('frontpage', '/')
+    config.add_route('all_posts', '/posts')
+    config.add_route('new_post', '/posts/new')
+    config.add_route('view_post', '/posts/{id}')
+    config.add_route('edit_post', '/posts/{id}/edit')
