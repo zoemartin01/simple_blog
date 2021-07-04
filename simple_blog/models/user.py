6bc52023eda074b5ac1e7e5803ad1c2215f75af2
Zoe Martin <git@zoemartin.me>
2021-07-04 10:27:37 +0200

    feat: added user model

---
+++ simple_blog/models/user.py
@@ -0,0 +1,27 @@
+import bcrypt
+from sqlalchemy import (
+    Column,
+    Integer,
+    Text,
+)
+
+from .meta import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(Text, nullable=False, unique=True)
+    username = Column(Text, nullable=False, unique=True)
+
+    password_hash = Column(Text)
+
+    def set_password(self, pw):
+        pw_hash = bcrypt.hashpw(pw.encode('utf8'), bcrypt.gensalt())
+        self.password_hash = pw_hash.decode('utf8')
+
+    def check_password(self, pw):
+        if self.password_hash is not None:
+            expected_hash = self.password_hash.encode('utf8')
+            return bcrypt.checkpw(pw.encode('utf8'), expected_hash)
+        return False
