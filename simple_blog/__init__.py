20e2a01b6ae352253a4279c68c7c8d77033fb548
Zoe Martin <git@zoemartin.me>
2021-07-04 10:41:44 +0200

    feat: added csrf protection

--- simple_blog/__init__.py
+++ simple_blog/__init__.py
@@ -6,6 +6,7 @@ def main(global_config, **settings):
     """
     with Configurator(settings=settings) as config:
         config.include('pyramid_mako')
+        config.include('.security')
         config.include('.routes')
         config.include('.models')
         config.scan()
