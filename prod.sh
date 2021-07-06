730b5ce730b384d57d31920ecbb9b3d7ad3ab0f8
Zoe Martin <git@zoemartin.me>
2021-07-06 15:36:51 +0200

    feat: add prod run script

---
+++ prod.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+alembic -c production.ini revision --autogenerate -m "init"
+alembic -c production.ini upgrade head
+initialize_simple_blog_db production.ini
+sqlite3 simple_blog.sqlite
+pserve production.ini
