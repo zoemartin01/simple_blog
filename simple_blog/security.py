fb3a754dd4abdfc24e20ae6481cd4762ce388c2e
Zoe Martin <git@zoemartin.me>
2021-07-06 15:01:38 +0200

    feat: added security stuff

--- simple_blog/security.py
+++ simple_blog/security.py
@@ -1,5 +1,42 @@
+from pyramid.authentication import AuthTktCookieHelper
 from pyramid.csrf import CookieCSRFStoragePolicy
+from pyramid.request import RequestLocalCache
+
+from . import models
+
+
+class MySecurityPolicy:
+    def __init__(self, secret):
+        self.authtkt = AuthTktCookieHelper(secret)
+        self.identity_cache = RequestLocalCache(self.load_identity)
+
+    def load_identity(self, request):
+        identity = self.authtkt.identify(request)
+        if identity is None:
+            return None
+
+        userid = identity['userid']
+        user = request.dbsession.query(models.User).get(userid)
+        return user
+
+    def identity(self, request):
+        return self.identity_cache.get_or_create(request)
+
+    def authenticated_userid(self, request):
+        user = self.identity(request)
+        if user is not None:
+            return user.id
+
+    def remember(self, request, userid, **kw):
+        return self.authtkt.remember(request, userid, **kw)
+
+    def forget(self, request, **kw):
+        return self.authtkt.forget(request, **kw)
 
 def includeme(config):
+    settings = config.get_settings()
+
     config.set_csrf_storage_policy(CookieCSRFStoragePolicy())
     config.set_default_csrf_options(require_csrf=True)
+
+    config.set_security_policy(MySecurityPolicy(settings['auth.secret']))
