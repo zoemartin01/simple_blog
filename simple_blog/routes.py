f4010cc4cfd0e636cea9d6d37311a3edbd640121
Zoe Martin <git@zoemartin.me>
2021-07-06 15:07:25 +0200

    feat: routing

--- simple_blog/routes.py
+++ simple_blog/routes.py
@@ -1,7 +1,16 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('frontpage', '/')
+
     config.add_route('all_posts', '/posts')
     config.add_route('new_post', '/posts/new')
     config.add_route('view_post', '/posts/{id}')
     config.add_route('edit_post', '/posts/{id}/edit')
+
+    config.add_route('login', '/login')
+    config.add_route('register', '/register')
+    config.add_route('logout', '/logout')
+    config.add_route('password_forgotten', '/password_forgotten/')
+    config.add_route('password_reset', '/password_forgotten/{token}')
+    config.add_route('activate', '/activate/{token}')
